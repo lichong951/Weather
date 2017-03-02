ce071775760af170c0af939cba38de562e0bfa20
lichong <lichong@luobotec.com>
2017-03-02 22:00:17 +0800

    add 加入创建数据库和表的各项配置

---
+++ app/src/main/java/top/lc951/weather/WeatherApplication.java
@@ -0,0 +1,18 @@
+package top.lc951.weather;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * @author lichong_951@163.com
+ * @version V1.0
+ * @Description: TODO
+ * @date 2017/3/2  21:56
+ * @powered by lichong
+ */
+
+public class WeatherApplication extends LitePalApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/top/lc951/weather/db/City.java
@@ -0,0 +1,50 @@
+package top.lc951.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @author lichong_951@163.com
+ * @version V1.0
+ * @Description: TODO
+ * @date 2017/3/2  21:49
+ * @powered by lichong
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
